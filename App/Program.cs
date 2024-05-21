7539008edccf59be394eac161bea8375414e9b2f
FabioPenedo <fabiopenedo21@gmail.com>
2024-05-21 12:30:54 -0300

    primeiro commit, extension methods

---
+++ App/Program.cs
@@ -0,0 +1,13 @@
+﻿using App.Extensions;
+
+namespace App // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new(2024, 05, 15, 5, 25, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
