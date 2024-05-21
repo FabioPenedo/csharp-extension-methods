5baaa5b2531d257447106266dc4a7db4d9e3c778
FabioPenedo <fabiopenedo21@gmail.com>
2024-05-21 12:40:32 -0300

    segundo exemplo: usando extension methods com string

--- App/Program.cs
+++ App/Program.cs
@@ -6,8 +6,11 @@ namespace App // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            DateTime dt = new(2024, 05, 15, 5, 25, 45);
-            Console.WriteLine(dt.ElapsedTime());
+            //DateTime dt = new(2024, 05, 15, 5, 25, 45);
+            //Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(11));
         }
     }
 }
